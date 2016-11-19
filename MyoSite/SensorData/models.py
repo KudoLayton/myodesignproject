f9e23e1dc9c1ff3e8a5eb2ed2b977ad967d9f92d
KudoLayton <lhslhg@gmail.com>
2016-11-19 23:02:12 +0900

    SensorData model setting error correction

--- MyoSite/SensorData/models.py
+++ MyoSite/SensorData/models.py
@@ -5,10 +5,10 @@ from django.utils import timezone
 
 class Data(models.Model):
 	#measure time
-	measureTime = DateTimeField(default=timezone.now)
+	measureTime = models.DateTimeField(default=timezone.now)
 
 	#temperature(celcious)
-	temperature = FloatField()
+	temperature = models.FloatField()
 
 	def __str__(self):
 		return self.measureTime
