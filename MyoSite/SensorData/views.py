69be8543d91373d75117fae88eed68a4f34713c3
KudoLayton <lhslhg@gmail.com>
2016-11-20 10:52:40 +0900

    test

--- MyoSite/SensorData/views.py
+++ MyoSite/SensorData/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from mjpegtools import MjpegParser
 
 def monitor(request):
-	image = MjpegParser(url='http://path-to-your-camera-mjpeg').serve()
+	image = MjpegParser(url='http://192.168.0.6:8080/test').serve()
 	return HttpResponse(image)
 	#return render(request, 'SensorData/monitor.html', {})
