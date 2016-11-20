a899c4ce7ea4665b3b7b356ff6c59ef258f617de
KudoLayton <lhslhg@gmail.com>
2016-11-20 16:42:08 +0900

    no message

--- MyoSite/SensorData/views.py
+++ MyoSite/SensorData/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 from mjpegtools import MjpegParser
 
 def monitor(request):
+	image = MjpegParser(url='http://192.168.0.6:8080/test.mjpg').serve()
+	print image
 	return render(request, 'SensorData/monitor.html', {})
