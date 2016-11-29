ba84178544b569b411fdea4e912bb0f645a92982
KudoLayton <lhslhg@gmail.com>
2016-11-30 01:26:33 +0900

    Revert "fix"
    This reverts commit 3b1817851557fb43404da7c6f2ebe667a241629c.

--- MyoSite/SensorData/static/js/data.js
+++ MyoSite/SensorData/static/js/data.js
@@ -1,16 +1,14 @@
-function getData(show){
+function getData(){
 	var xhr = new XMLHttpRequest();
 	xhr.open('post', 'getdata/');
-	xhr.onreadystatechange = show;
-	xhr.send(null);
-}
-
-function showData(){
-	if(xhr.readyState === 4){
-		if(xhr.status === 200){
-			data = JSON.parse(xhr.responseText);
-			document.getElementById("temperature").innerHTML = data[0].fields.temperature; 
-			setInterval(getData, 1000);
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4){
+			if(xhr.status === 200){
+				data = JSON.parse(xhr.responseText);
+				document.getElementById("temperature").innerHTML = data[0].fields.temperature; 
+				setInterval(getData, 1000);
+			}
 		}
 	}
+	xhr.send(null);
 }
