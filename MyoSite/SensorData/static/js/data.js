ee08e59014c85e06f1a8a7c84ccdf40bc764d090
KudoLayton <lhslhg@gmail.com>
2016-11-30 01:50:01 +0900

    fix

--- MyoSite/SensorData/static/js/data.js
+++ MyoSite/SensorData/static/js/data.js
@@ -5,7 +5,7 @@ function getData(){
 		if(xhr.readyState === 4){
 			if(xhr.status === 200){
 				data = JSON.parse(xhr.responseText);
-				document.getElementById("temperature").innerHTML = data[0].fields.temperature; 
+				document.getElementById("temperature").innerHTML = data[0].fields.temperature+"°C"; 
 			}
 		}
 	}
