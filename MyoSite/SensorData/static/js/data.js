3b1817851557fb43404da7c6f2ebe667a241629c
KudoLayton <lhslhg@gmail.com>
2016-11-30 01:24:49 +0900

    fix

--- MyoSite/SensorData/static/js/data.js
+++ MyoSite/SensorData/static/js/data.js
@@ -1,14 +1,16 @@
-function getData(){
+function getData(show){
 	var xhr = new XMLHttpRequest();
 	xhr.open('post', 'getdata/');
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState === 4){
-			if(xhr.status === 200){
-				data = JSON.parse(xhr.responseText);
-				document.getElementById("temperature").innerHTML = data[0].fields.temperature; 
-				setInterval(getData, 1000);
-			}
+	xhr.onreadystatechange = show;
+	xhr.send(null);
+}
+
+function showData(){
+	if(xhr.readyState === 4){
+		if(xhr.status === 200){
+			data = JSON.parse(xhr.responseText);
+			document.getElementById("temperature").innerHTML = data[0].fields.temperature; 
+			setInterval(getData, 1000);
 		}
 	}
-	xhr.send(null);
 }
