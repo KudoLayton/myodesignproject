bec32833fed2acf67b7f3b1c10eabfe3916b7f20
YYS8646-PC\YYS8646 <https://github.com/YoonYoungSeok>
2016-12-04 00:01:05 +0900

    dumy data store and streaming successful

--- MyoNucMotor/MyoNucMotor/main.cpp
+++ MyoNucMotor/MyoNucMotor/main.cpp
@@ -18,8 +18,10 @@
 #include <stack>
 #include <mutex>
 #include "data.pb.h"
+// send to query
+#include "curl/curl.h"
 
-//#define PORT_NAME L"COM5"
+#define PORT_NAME L"COM3"
 //#define PORT_NAME L"\\\\.\\COM14"
 
 #define Car_L 15
@@ -28,9 +30,8 @@
 #define rpm2cm_s (3*3.141592/60)
 #define motor_delay 20
 
-int main() {		// Myo, Serial, Socket
+int main() {		// Myo, Serial, curl
 	try {
-		// myo connect
 		myo::Hub hub("com.example.hello-myo");
 		hub.setLockingPolicy(myo::Hub::lockingPolicyNone);
 
@@ -44,13 +45,17 @@ int main() {		// Myo, Serial, Socket
 
 		DataCollector collector;
 		hub.addListener(&collector);
-
+		//myo::Hub hub("com.example.hello-myo");
+		//hub.setLockingPolicy(myo::Hub::lockingPolicyNone);
+		//MyoData myodata(hub);
 
 		// serial open
 		CSerialPort port;
 //		if (!port.Open(PORT_NAME, CBR_115200, 8, ONESTOPBIT, NOPARITY))
-		if (!port.Open(L"COM3", CBR_115200, 8, ONESTOPBIT, NOPARITY))
-			if (!port.Open(L"\\\\.\\COM17", CBR_115200, 8, ONESTOPBIT, NOPARITY))
+		//if (!port.Open(L"COM3", CBR_115200, 8, ONESTOPBIT, NOPARITY))
+		//	if (!port.Open(L"\\\\.\\COM17", CBR_115200, 8, ONESTOPBIT, NOPARITY))
+		//		return 1;
+		if (!port.Open(L"COM3", CBR_9600, 8, ONESTOPBIT, NOPARITY))
 				return 1;
 		port.SetTimeout(10, 10, 1);
 
@@ -78,6 +83,11 @@ int main() {		// Myo, Serial, Socket
 
 		float Lmove = 0;
 		float Rmove = 0;
+
+		CURL *curl;
+		CURLcode res;
+		curl_global_init(CURL_GLOBAL_ALL);
+
 		while (1) {
 			// myo comm
 			hub.run(1000 / 20);
@@ -123,12 +133,11 @@ int main() {		// Myo, Serial, Socket
 
 			std::string buff2 = buff1.substr(buff1.find_first_of('\n') + 1);
 			std::string buff3 = buff2.substr(buff2.find_first_of('\n') + 1);
-/*
-			std::cout << "Total Read: " << buff1.substr(0, n) << std::endl;
 
-			std::cout << "buff1: " << buff1.substr(0, buff1.find_first_of("\n")) << std::endl;
-			std::cout << "buff2: " << buff2.substr(0, buff2.find_first_of("\n")) << std::endl;
-			*/
+//			std::cout << "Total Read: " << buff1.substr(0, n) << std::endl;
+
+//			std::cout << "buff1: " << buff1.substr(0, buff1.find_first_of("\n")) << std::endl;
+//			std::cout << "buff2: " << buff2.substr(0, buff2.find_first_of("\n")) << std::endl;
 
 			if ( buff3.find("\n") != std::string::npos){
 //				std::cout << "buff3: " << buff3.substr(0, buff3.find_first_of("\n")) << std::endl;
@@ -140,132 +149,26 @@ int main() {		// Myo, Serial, Socket
 						buff2.substr(0, buff2.find_first_of("\n"))	;
 
 //				std::string s = buff1.substr(0, buff1.find_first_of("\n"));
-				std::cout << "Parsed: " << s << std::endl;
-
-//				try {
-///*					if( Lmove == 0 )
-//						sensor.set_arg0((float) (-1));
-//					else
-//						sensor.set_arg0((float) Lmove);
-//					if (Rmove == 0)
-//						sensor.set_arg1((float)(-1));
-//					else
-//						sensor.set_arg1((float) Rmove);
-//
-//					std::cout << Lmove << '\t' << Rmove << std::endl;
-//*/
-//					for (int i = 0; i < 8; i++) {
-////						std::cout << i << ':' << s << std::endl;
-//						s = sensor_parsing(&sensor, s, i, &Lmove, &Rmove);
-//					}
-///*
-//					if (s.find(",") != std::string::npos) {
-//						f = std::stof(s.substr(0, s.find_first_of(',')));
-//						f = f == 0 ? -1 : f;
-//						sensor.set_arg2(f);
-////						std::cout << "arg0: " << f << std::endl;
-//						s = s.substr(s.find_first_of(',') + 1);
-//					}
-//					else {
-//						f = std::stof(s.substr(0, s.find_first_of('\n')));
-//						f = f == 0 ? -1 : f;
-//						sensor.set_arg2(f);
-////						std::cout << "arg0: " << f << std::endl;
-//					}
-//
-//					if (s.find(",") != std::string::npos) {
-//						f = std::stof(s.substr(0, s.find_first_of(',')));
-//						f = f == 0 ? -1 : f;
-//						sensor.set_arg3(f);
-////						std::cout << "arg1: " << f << std::endl;
-//						s = s.substr(s.find_first_of(',') + 1);
-//					}
-//					else {
-//						f = std::stof(s.substr(0, s.find_first_of('\n')));
-//						f = f == 0 ? -1 : f;
-//						sensor.set_arg3(f);
-////						std::cout << "arg1: " << f << std::endl;
-//					}
-//
-//					if (s.find(",") != std::string::npos) {
-//						f = std::stof(s.substr(0, s.find_first_of(',')));
-//						Lmove += f;
-//						f = f == 0 ? -1 : f;
-//						sensor.set_arg4(f);
-////						std::cout << "arg2: " << f << std::endl;
-//						s = s.substr(s.find_first_of(',') + 1);
-//					}
-//					else {
-//						f = std::stof(s.substr(0, s.find_first_of('\n')));
-//						Lmove += f;
-//						f = f == 0 ? -1 : f;
-//						sensor.set_arg4(f);
-////						std::cout << "arg2: " << f << std::endl;
-//					}
-//
-//					if (s.find(",") != std::string::npos) {
-//						f = std::stof(s.substr(0, s.find_first_of(',')));
-//						Rmove += f;
-//						f = f == 0 ? -1 : f;
-//						sensor.set_arg5(f);
-////						std::cout << "arg3: " << f << std::endl;
-//						s = s.substr(s.find_first_of(',') + 1);
-//					}
-//					else {
-//						f = std::stof(s.substr(0, s.find_first_of('\n')));
-//						Rmove += f;
-//						f = f == 0 ? -1 : f;
-//						sensor.set_arg5(f);
-////						std::cout << "arg3: " << f << std::endl;
-//						f = 0;
-//					}
-//
-//					if (s.find(",") != std::string::npos) {
-//						f = std::stof(s.substr(0, s.find_first_of(',')));
-//						f = f == 0 ? -1 : f;
-//						sensor.set_arg6(f);
-////						std::cout << "arg4: " << f << std::endl;
-//						s = s.substr(s.find_first_of(',') + 1);
-//					}
-//					else {
-//						f = std::stof(s.substr(0, s.find_first_of('\n')));
-//						f = f == 0 ? -1 : f;
-//						sensor.set_arg6(f);
-////						std::cout << "arg4: " << f << std::endl;
-//					}
-//
-//					sensor.set_arg7((float) (-1));
-//					*/
-//
-////					std::lock_guard<std::mutex> guard(tcpBuffer_mut);
-////					tcpBuffer_mut.lock();
-//					while (1)
-//						if (tcpBuffer_mut.try_lock())
-//							break;
-//					sensor.SerializeToArray(tcpBuffer, BUFSIZ);
-//					tcpBuffer_mut.unlock();
-//
-//					//message send
-//				}
-//				catch (std::exception e) {
-//					std::cout << "parsing error!" << std::endl;
-//					std::cout << "buff1: " << buff1.substr(0, buff1.find_first_of("\n")) << std::endl;
-//					std::cout << "buff2: " << buff2.substr(0, buff2.find_first_of("\n")) << std::endl;
-//					std::cout << "buff3: " << buff3.substr(0, buff3.find_first_of("\n")) << std::endl;
-////					std::cout << " (" << n << ')' << "\n";
-////					std::cout << " (" << buff1.find_first_of("\n") << ')' << "\n";
-//					std::cout << "Parsed: " << s << std::endl;
-//				}
+//				std::cout << "Parsed: " << s << std::endl;
 			}
 //			std::cout << " (" << n << ')' << "\n";
+
+			std::string buffn = buff2.substr(0, buff2.find_first_of("\n"));
+//			if (!buffn.empty()) buffn.erase('\n');
+
+			std::cout << "buffn: " << buffn << std::endl;
+
+			curl = curl_easy_init();
+			if (curl) {
+				curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.0.83:8000/input/");
+//				curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "temp=28.2");
+				curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buffn.c_str());
+				res = curl_easy_perform(curl);
+				if (res != CURLE_OK)
+					fprintf(stderr, "curl_Easy_perform() failed: %s\n", curl_easy_strerror(res));
+				curl_easy_cleanup(curl);
+			}
 		}
-		//std::thread *p;
-		//while (!clientThreads.empty()) {
-		//	p = clientThreads.top();
-		//	p->join();
-		//	clientThreads.pop();
-		//}
-		//t.join();
 	}
 	catch (const std::exception& e) {
 		std::cerr << "Error: " << e.what() << std::endl;
@@ -274,4 +177,5 @@ int main() {		// Myo, Serial, Socket
 		return 1;
 	}
 
+	curl_global_cleanup();
 }
