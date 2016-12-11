65dc73bf8c9648d9830d50023210d7d28ef5d751
KudoLayton <lhslhg@gmail.com>
2016-12-12 02:03:23 +0900

    cure

--- MyoSite/SensorData/static/js/data.js
+++ MyoSite/SensorData/static/js/data.js
@@ -4,38 +4,39 @@ function initDraw(barsrc){
 	var avrVelCanvas = document.getElementById('avrVel');
 	var avctx = avrVelCanvas.getContext('2d');
 	var bar = new Image();
-	var percent = 0;
 	bar.onload = function(){
-		cvctx.strokeRect(0, canvas.height / 2 - canvas.height/2, canvas.width, canvas.height);
-		avctx.strokeRect(0, canvas.height / 2 - canvas.height/2, canvas.width, canvas.height);
-		cvctx.drawImage (bar, 0, 0, 0, bar.height, 0, canvas.height / 2 - canvas.height/2, 0, canvas.height);
-		avctx.drawImage (bar, 0, 0, 0, bar.height, 0, canvas.height / 2 - canvas.height/2, 0, canvas.height);
+		cvctx.strokeRect(0, 0, curVelCanvas.width, curVelCanvas.height);
+		avctx.strokeRect(0, 0, avrVelCanvas.width, avrVelCanvas.height);
+		cvctx.drawImage (bar, 0, 0, 0, bar.height, 0, 0, 0, curVelCanvas.height);
+		avctx.drawImage (bar, 0, 0, 0, bar.height, 0, 0, 0, avrVelCanvas.height);
 	};
 	bar.src = barsrc;
-
-	function getData(){
-		var xhr = new XMLHttpRequest();
-		xhr.open('post', 'getdata/');
-		var percent;
-		xhr.onreadystatechange = function(){
-			if(xhr.readyState === 4){
-				if(xhr.status === 200){
-					data = JSON.parse(xhr.responseText);
-					document.getElementById("temperature").innerHTML = data[0].fields.temperature+"°C";
-					document.getElementById("pressure").innerHTML = data[0].fields.pressure+"hPa";
-					percent = data[0].fields.curVel / 3.5;
-					cvctx.clearRect(0, 0, canvas.height, canvas.width);
-					cvctx.strokeRect(0, canvas.height / 2 - canvas.height/2, canvas.width, canvas.height);
-					cvctx.drawImage (bar, 0, 0, bar.width * percent, bar.height, 0, canvas.height / 2 - canvas.height/2, canvas.width * percent, canvas.height);
-					percent = data[0].fields.aveVel / 3.5;
-					avctx.clearRect(0, 0, canvas.height, canvas.width);
-					avctx.strokeRect(0, canvas.height / 2 - canvas.height/2, canvas.width, canvas.height);
-					avctx.drawImage (bar, 0, 0, bar.width * percent, bar.height, 0, canvas.height / 2 - canvas.height/2, canvas.width * percent, canvas.height);
-				}
-			}
-		}
-		xhr.send(null);
-	}
+	return bar;
 }
 
-
+function getData(bar){
+	var xhr = new XMLHttpRequest();
+	xhr.open('post', 'getdata/');
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4){
+			if(xhr.status === 200){
+				data = JSON.parse(xhr.responseText);
+				document.getElementById("temperature").innerHTML = data[0].fields.temperature+"°C";
+				document.getElementById("pressure").innerHTML = data[0].fields.pressure+"hPa";
+				var cvpercent = data[0].fields.curVel / 3.5;
+				var avpercent = data[0].fields.aveVel / 3.5;
+				var curVelCanvas = document.getElementById('curVel');
+				var cvctx = curVelCanvas.getContext('2d');
+				var avrVelCanvas = document.getElementById('avrVel');
+				var avctx = avrVelCanvas.getContext('2d');
+				cvctx.clearRect(0, 0, curVelCanvas.height, curVelCanvas.width);
+				cvctx.strokeRect(0, 0, curVelCanvas.width, curVelCanvas.height);
+				cvctx.drawImage (bar, 0, 0, bar.width * cvpercent, bar.height, 0, 0, curVelCanvas.width * cvpercent, curVelCanvas.height);
+				avctx.clearRect(0, 0, avrVelCanvas.height, avrVelCanvas.width);
+				avctx.strokeRect(0, 0, avrVelCanvas.width, avrVelCanvas.height);
+				avctx.drawImage (bar, 0, 0, bar.width * avpercent, bar.height, 0, 0, avrVelCanvas.width * avpercent, avrVelCanvas.height);
+			}
+		}
+	}
+	xhr.send(null);
+}
